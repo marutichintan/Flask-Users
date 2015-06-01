b1e7745df9b54a4b4502fd816a2a3e28991dabc7
Leo <leo@leog.in>
2015-06-02 00:04:51 +0530

    tests

--- tests.py
+++ tests.py
@@ -11,39 +11,41 @@ app = create_app('config')
 class TestUsers(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
-        self.add_success = False
-        
-    def test_list(self): 
-      self.app = app.test_client()    
+
+
+    def test_01_list(self):
+      self.app = app.test_client()
       rv = self.app.get('/users/')
       assert "Users" in rv.data.decode('utf-8')
-      
-             
-    def test_add(self):
-        rv = self.app.post('/users/add', data=dict(name = 'test name', email = 'test@email.com'), follow_redirects=True)
-        self.add_success = True
+
+
+    def test_05_add(self):
+        rv = self.app.post('/users/add', data=dict(name = 'test name', email = 'test@email.com',
+                           password="qwe765", is_enabled=True, role="None"), follow_redirects=True)
         assert 'Add was successful' in rv.data.decode('utf-8')
-    
-     
-    @unittest.skipUnless(self.add_success)        
-    def test_Update(self):
+
+
+
+    def test_10_Update(self):
+
          with app.app_context():
-            id = Users.query.first().id
-            rv = self.app.post('/users/update/{}'.format(id), data=dict(name = 'test name update', email = 'test@email.update'), follow_redirects=True)
+            user = Users.query.filter_by(email='test@email.com').first()
+            id = user.id
+            rv = self.app.post('/users/update/{}'.format(id), data=dict(name = 'test name update',
+                               email = 'test@email.update', password="qwe75", is_enabled=False, role="admin"),
+                               follow_redirects=True)
             assert 'Update was successful' in rv.data.decode('utf-8')
 
-    """def test_delete(self):
-               
-                    rv = self.app.post('/users/delete/{}'.format(id), follow_redirects=True)
-                    assert 'Delete was successful' in rv.data.decode('utf-8')
-        """
-     
-    
-        
-   
-     
+    def test_15_delete(self):
+                     with app.app_context():
+                       id = Users.query.first().id
+                       rv = self.app.post('/users/delete/{}'.format(id), follow_redirects=True)
+                       assert 'Delete was successful' in rv.data.decode('utf-8')
+
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
-    
-    
-    
