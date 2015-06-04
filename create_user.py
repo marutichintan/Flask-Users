4646f730362e0e9cc98126f0c9556d875a7b7127
Leo <leo@leog.in>
2015-06-05 02:14:14 +0530

    fixed tests and create_users

--- create_user.py
+++ create_user.py
@@ -12,15 +12,22 @@ password=generate_password_hash('password')
 is_enabled=True
 role="admin"
 user=Users(email, name,password, is_enabled)
-with app.app_context():
-    role = Roles.query.filter_by(name=role).first()
-    user.roles.append(role)
-    db.session.add(user)
 
-
+def db_commit():
     try:
           db.session.commit()
           print("{} was added successfully".format(email))
+          return True
     except SQLAlchemyError as e:
           reason=str(e)
           print (reason)
+          return False
+
+with app.app_context():
+    new_role = Roles(role)
+    db.session.add(new_role)
+    if db_commit():
+        user_role = Roles.query.filter_by(name=role).first()
+        user.roles.append(user_role)
+        db.session.add(user)
+        db_commit()
